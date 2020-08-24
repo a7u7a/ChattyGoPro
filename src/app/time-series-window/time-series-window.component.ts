d369840b64bfe9a7bfe5ad9cdf6a8fc5a8729d4b
eserrano <eserrano@exseed.ed.ac.uk>
2020-08-24 11:04:35 -0400

    wip

--- src/app/time-series-window/time-series-window.component.ts
+++ src/app/time-series-window/time-series-window.component.ts
@@ -3,6 +3,7 @@
 
 import { Component, OnInit, ViewEncapsulation, ElementRef } from '@angular/core';
 import * as d3 from 'd3';
+import { timeFormat } from 'd3';
 
 @Component({
   selector: 'app-time-series-window',
@@ -12,16 +13,37 @@ import * as d3 from 'd3';
 })
 export class TimeSeriesWindowComponent implements OnInit {
   hostElement; // Native element hosting the container
+  
+  // Dataset
   dataset;
   metricCount;
   metricMonths;
+
+  // Chart sizing
+  optwidth = 800;
+  optheight = 370;
   margin;
   width;
   height;
   margin_context;
   height_context;
-  optwidth = 800; // Chart w size
-  optheight = 370; // Chart h size
+  
+  // Data ranges
+  dataXrange;
+  dataYrange;
+  mindate;
+  maxdate;
+  DateFormat;
+  dynamicDateFormat;
+
+  // Axis
+  x:any;
+  y;
+  xAxis;
+  yAxis;
+  x2;
+  y2;
+  xAxis_context;
 
   constructor(private elRef: ElementRef) {
     this.hostElement = this.elRef.nativeElement;
@@ -33,6 +55,87 @@ export class TimeSeriesWindowComponent implements OnInit {
 
 private createChart(){
 
+  this.setChartDimensions();
+
+  this.prepareData();
+
+  this.setDataRanges();
+
+  this.createAxis();
+
+  console.log(this.dataset)
+
+  
+  
+  
+
+}
+
+private createAxis(){
+
+  // Focus chart
+  this.x = d3.scaleTime() // not sure if this is working right
+      .range([0,(this.width)])
+      .domain(this.dataXrange);
+  
+  this.y = d3.scaleLinear()
+      .range([this.height, 0])
+      .domain(this.dataYrange);
+  
+  this.xAxis = d3.axisBottom(this.x)
+      .tickSize(-(this.height))
+      .ticks(this.customTickFunction)
+      .tickFormat(this.dynamicDateFormat);
+
+  this.yAxis = d3.axisRight(this.y)
+      .ticks(4)
+      .tickSize(-(this.width));
+  
+  // Context chart
+  this.x2 = d3.scaleTime()
+      .range([0, this.width])
+      .domain([this.mindate, this.maxdate]);
+  
+  this.y2 = d3.scaleLinear()
+      .range([this.height_context,0])
+      .domain(this.y.domain());
+
+  this.xAxis_context = d3.axisBottom(this.x2)
+      .ticks(this.customTickFunction)
+      .tickFormat(this.dynamicDateFormat);
+}
+
+
+private setDataRanges(){
+
+  // Compute range of available data
+  this.dataXrange = d3.extent(this.dataset, function(d:any) { return d.month; });
+  this.dataYrange = [0, d3.max(this.dataset, function(d:any) { return d.count; })];
+   
+  // Maximum date range allowed to display
+  this.mindate = this.dataXrange[0];
+  this.maxdate = this.dataXrange[1];
+
+  this.DateFormat = d3.timeFormat("%b %Y");
+  
+  this.dynamicDateFormat = this.timeFormat([
+    [d3.timeFormat("%Y"), function() { return true; }], // <-- how to display when Jan 1 YYYY
+    [d3.timeFormat("%b %Y"), function(d) { return d.getMonth(); }],
+    [function() { return ""; }, function(d) { return d.getdDate() != 1; }]
+  ]);
+}
+
+
+private timeFormat(formats){
+  return function(date) {
+    var i = formats.length - 1, f = formats[i];
+    while (!f[1](date)) f = formats[--i];
+    return f[0](date);
+  };
+}
+
+private setChartDimensions(){
+
   // Set focus chart size
   this.margin = {top: 20, right: 30, bottom: 100, left: 20};
   this.width = this.optwidth - this.margin.left - this.margin.right;
@@ -41,12 +144,8 @@ private createChart(){
   // Set Context chart size
   this.margin_context = {top: 320, right: 30, bottom: 20, left: 20};
   this.height_context = this.optheight - this.margin_context.top - this.margin_context.bottom;
-
-
-
-  this.prepareData();
-  console.log(this.dataset)
 }
+
 
   private prepareData(){
 
@@ -63,7 +162,7 @@ private createChart(){
     }
     
     // Format month as a date
-    var parseTime = d3.timeParse("%Y-%m");
+    var parseTime = d3.timeParse("%Y-%m"); // used instead of d.month = d3.time.format("%Y-%m").parse(d.month);
     this.dataset.forEach(function (d){
       d.month = parseTime(d.month);
     })
@@ -72,8 +171,27 @@ private createChart(){
     this.dataset.sort(function(x, y){
       return d3.ascending(x.month, y.month);
     })
-
   }
 
+  private customTickFunction(t0,t1,dt){
+    var labelSize = 42;
+    var maxTotalLabels = Math.floor(this.width / labelSize);
+
+    function step(date,offset) {
+      date.setMonth(date.getMonth() + offset);
+    }
+    var time = d3.timeMonth.ceil(t0);
+    var times = [];
+    var monthFactors = [1,3,4,12];
+
+    while(time < t1) times.push(new Date(+time)),step(time, 1);
+    var timesCopy = times;
+    var i;
+    for(i = 0; times.length > maxTotalLabels; i++)
+      times = timesCopy.filter(function(d){
+        return (d.getMonth()) % monthFactors[i] == 0;
+      });
+      return times;
+  };
 
 }
