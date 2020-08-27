281732d2fae6194070a8a25570134aad38b664aa
eserrano <eserrano@exseed.ed.ac.uk>
2020-08-27 00:21:11 -0400

    wip

--- src/app/line-child/line-child.component.ts
+++ src/app/line-child/line-child.component.ts
@@ -17,6 +17,9 @@ height;
 x;
 y;
 data;
+sumstat;
+paths;
+
 
   constructor(private elRef: ElementRef,private http: HttpClient) { 
     this.hostElement = this.elRef.nativeElement;
@@ -26,56 +29,102 @@ data;
 
   ngOnInit(): void {
     // Create chart once data has been loaded
-    this.http.get("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+    this.http.get("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv",
     { responseType: 'text' }).subscribe(data => {
-    var objs = d3.csvParse(data, 
-      function(d){
-        return { date: d3.timeParse("%Y-%m-%d")(d.date),value:d.value}
-      });
+    var objs = d3.csvParse(data);
     this.createChart(objs);
     });
 
-    this.margin = {top: 10, right:10, bottom:10, left: 25};
+    this.margin = {top: 10, right:30, bottom:30, left: 60};
   }
 
   private createChart(objs){
-    console.log(objs[0]);
     this.data = objs;
     this.setChart();
 
+    this.sumstat = d3.nest()
+        .key(function(d:any)  { return d.name; })
+        .entries(this.data);
+
     // Create X axis
-    this.x = d3.scaleTime()
-        .domain(d3.extent(this.data, (d:any) => {return d.date})) // works even if highlighted
+    this.x = d3.scaleLinear()
+        .domain(d3.extent(this.data, (d:any) => { return d.year; }))
         .range([0, this.width]);
     this.svg.append("g")
         .attr("transform", "translate(" + 0 + " " +  this.height +")")
         .attr("stroke-width", 0.5)
-        .call(d3.axisBottom(this.x));
+        .call(d3.axisBottom(this.x).ticks(5));
 
     // Create Y axis
     this.y = d3.scaleLinear()
-        .domain([0, d3.max(this.data, function(d:any) { return +d.value; })])
+        .domain([0, d3.max(this.data, function(d:any){return +d.n; })])
         .range([this.height, 0]);
     this.svg.append("g")
         .call(d3.axisLeft(this.y));
-        console.log("henlo",this.x(1));
+
+    // color palette
+    var res = this.sumstat.map(function(d){ return d.key }) // list of group names
+    var color = d3.scaleOrdinal()
+                  .domain(res)
+                  .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
+
+                  //console.log("henlo",this.sumstat[0].values[0].n);
+
+    //console.log(this.sumstat);
+    // Draw the line
+    this.paths = [];
+
+
+
+    this.svg.selectAll(".line")
+    .data(this.sumstat)
+     .enter()
+     .append("path")
+     .attr("fill", "none")
+     .attr("stroke", d => { return color(d.key) })
+     .attr("stroke-width", 1.5)
+     .attr("d", function(d) {
+
+       return d3.line()
+       .x((d:any) => { console.log("hola", d.year); return this.x(d.year); }) //x q no imprime esos valores ??
+       .y((d:any) => { return this.y(+d.n); })
+       (d.values); // x q arroja -> TypeError: this.x is not a function ??
+       
+     })
+
+    // Create X axis
+    // this.x = d3.scaleTime()
+    //     .domain(d3.extent(this.data, (d:any) => {return d.date})) // works
+    //     .range([0, this.width]);
+    // this.svg.append("g")
+    //     .attr("transform", "translate(" + 0 + " " +  this.height +")")
+    //     .attr("stroke-width", 0.5)
+    //     .call(d3.axisBottom(this.x));
+
+    // Create Y axis
+    // this.y = d3.scaleLinear()
+    //     .domain([0, d3.max(this.data, function(d:any) { return +d.value; })])
+    //     .range([this.height, 0]);
+    // this.svg.append("g")
+    //     .call(d3.axisLeft(this.y));
+    //     console.log("henlo",this.x(1));
         
 
     // Add line
-    this.svg.append("path")
-        .datum(this.data)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1)
-        .attr("d", d3.line()
-          .x((d:any) => { return this.x(d.date)})
-          .y((d:any) => { return this.y(d.value)})
-        );
+    // this.svg.append("path")
+    //     .datum(this.data)
+    //     .attr("fill", "none")
+    //     .attr("stroke", "steelblue")
+    //     .attr("stroke-width", 1)
+    //     .attr("d", d3.line()
+    //       .x((d:any) => { return this.x(d.date)})
+    //       .y((d:any) => { return this.y(d.value)})
+    //     );
 
 
       
 
-        this.svg.attr("transform", "translate(30,10)");
+    this.svg.attr("transform", "translate(30,10)");
   }
 
   private setChart(){
@@ -90,7 +139,7 @@ data;
         .attr('height', this.height + this.margin.top + this.margin.bottom)
         .attr('viewBox', '0 0 ' + viewBoxWidth + ' ' + viewBoxHeight)
         .append('g')
-        .attr("transform", "translate(0,0)");
+        .attr("transform", "translate("+this.margin.left + this.margin.top +")");
 
 
   }
