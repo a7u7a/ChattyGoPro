1d46d0b348e89c06180c005c6cc38f161fd7b97c
eserrano <eserrano@exseed.ed.ac.uk>
2020-08-27 11:40:13 -0400

    cleanup

--- src/app/line-child/line-child.component.ts
+++ src/app/line-child/line-child.component.ts
@@ -29,6 +29,10 @@ paths;
 
   ngOnInit(): void {
     // Create chart once data has been loaded
+
+    // Test with raw file
+    // https://raw.githubusercontent.com/a7u7a/dummydata/master/gyroscope/gyro_1.csv
+
     this.http.get("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv",
     { responseType: 'text' }).subscribe(data => {
     var objs = d3.csvParse(data);
@@ -62,19 +66,11 @@ paths;
     this.svg.append("g")
         .call(d3.axisLeft(this.y));
 
-    // color palette
+    // Color palette
     var res = this.sumstat.map(function(d){ return d.key }) // list of group names
     var color = d3.scaleOrdinal()
                   .domain(res)
                   .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
-
-                  //console.log("henlo",this.sumstat[0].values[0].n);
-
-    //console.log(this.sumstat);
-    // Draw the line
-    this.paths = [];
-
-
 
     this.svg.selectAll(".line")
     .data(this.sumstat)
@@ -91,35 +87,6 @@ paths;
        return lineFunction(d.values); // x q arroja -> TypeError: this.x is not a function ??
 
      })
-
-    // Create X axis
-    // this.x = d3.scaleTime()
-    //     .domain(d3.extent(this.data, (d:any) => {return d.date})) // works
-    //     .range([0, this.width]);
-    // this.svg.append("g")
-    //     .attr("transform", "translate(" + 0 + " " +  this.height +")")
-    //     .attr("stroke-width", 0.5)
-    //     .call(d3.axisBottom(this.x));
-
-    // Create Y axis
-    // this.y = d3.scaleLinear()
-    //     .domain([0, d3.max(this.data, function(d:any) { return +d.value; })])
-    //     .range([this.height, 0]);
-    // this.svg.append("g")
-    //     .call(d3.axisLeft(this.y));
-    //     console.log("henlo",this.x(1));
-        
-
-    // Add line
-    // this.svg.append("path")
-    //     .datum(this.data)
-    //     .attr("fill", "none")
-    //     .attr("stroke", "steelblue")
-    //     .attr("stroke-width", 1)
-    //     .attr("d", d3.line()
-    //       .x((d:any) => { return this.x(d.date)})
-    //       .y((d:any) => { return this.y(d.value)})
-    //     );
 
     this.svg.attr("transform", "translate(0,0)");
   }
