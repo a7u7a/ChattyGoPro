9cea5239db253e4ac9eea8303bf27a9e379cf3b1
eserrano <eserrano@exseed.ed.ac.uk>
2020-10-05 09:05:18 -0300

    updated edin config file

--- src/environments/environment.edin.ts
+++ src/environments/environment.edin.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  apiUrl: 'http://172.16.50.221:7000',
+  // apiUrl: 'http://172.16.50.221:7000',
+  apiUrl: 'http://129.215.193.117:7000',
   websocketUrl: 'ws://172.16.50.221',
   production: true
 };
